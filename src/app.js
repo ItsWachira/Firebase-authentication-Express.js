6e5c80bb8f73973cf8dc8a3165a36d1ec6b2b469
ItsWachira <brianwachira79@gmail.com>
2024-10-08 14:48:02 +0300

    fix: update cors config bug fixes

--- src/app.js
+++ src/app.js
@@ -9,29 +9,28 @@ const SERVER_HOST = process.env.SERVER_HOST || '0.0.0.0';
 
 const app = express();
 
-// Apply CORS middleware before the router
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'], // Allow access from localhost:3000 and localhost:5173
+    origin: ['http://localhost:5173'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true  // Allow credentials like cookies or auth headers
+    credentials: true  
 }));
 
-// Parse JSON and cookies
+
 app.use(express.json());
 app.use(cookieParser());
 
-// Use your router
+
 app.use(router);
 
-// Enable pre-flight requests for all routes (if necessary)
+
 app.options('*', cors());
 
-// Sample route
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-// Start the server, binding to both the host and port
+
 app.listen(SERVER_PORT, SERVER_HOST, () => {
     console.log(`Listening on ${SERVER_HOST}:${SERVER_PORT}`);
 });
