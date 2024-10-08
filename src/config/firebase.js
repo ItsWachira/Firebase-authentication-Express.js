81c3f1e5c954f119b4c2d9a80d8e72bae8775c66
ItsWachira <brianwachira79@gmail.com>
2024-10-08 15:10:37 +0300

    add vercel.json configs

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -12,7 +12,7 @@ const {
 } = require("firebase/auth") ;
 
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString());
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
